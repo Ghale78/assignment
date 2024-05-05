5c9487a0e7cf7a650db2bb09abcfae190201c832
Ghale <kapilghale78@gmail.com>
2024-05-05 22:47:49 +0545

    Homework Updated

--- react/practice/src/app/facebook/page.js
+++ react/practice/src/app/facebook/page.js
@@ -4,18 +4,22 @@ import { GrLike } from "react-icons/gr";
 //click ->skyblue
 //re-click -> back to grey
 const page = () => {
-
-    const [color, setColor]= useState('red')
-    const handleColorChange = ()=>{
-    setColor('blue')
-    }
-  return (
-    <div>
-        <button onClick={()=>handleColorChange()} className='bg-gray-200 p-2 border border-black'>
-            <GrLike color={color}/>
-            </button>
-    </div>
-  )
+  let [color, setColor]= useState('grey')
+  
+  const handleColorChange = ()=>{
+          if(color=='grey'){
+            setColor('skyblue')
+          }else{
+            setColor('grey')
+          }
+  }
+return (
+  <div>
+      <button onClick={()=>handleColorChange()} className='bg-gray-200 p-2 border border-black'>
+          <GrLike color={color}/>
+          </button>
+  </div>
+)
 }
 
 export default page
